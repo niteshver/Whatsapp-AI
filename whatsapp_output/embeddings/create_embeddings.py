886e4cdfbc5577197acf09c4d30a3843bafe8a40
niteshver <niteshv1520@gmail.com>
2025-12-24 20:50:41 +0530

    import embedding data and create virtual environment

---
+++ whatsapp_output/embeddings/create_embeddings.py
@@ -0,0 +1,6 @@
+from langchain_community.embeddings import OllamaEmbeddings
+from langchain_community.vectorstores import FAISS
+import json
+from langchain_core.documents import Document
+
+with open(WHA)
